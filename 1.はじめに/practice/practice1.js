5d84416394bd7b0203fa936dea91576696fdf825
asukaokochi <x17018xx@gmail.com>
2018-11-22 17:36:17 +0900

    first-commit

---
+++ 1.はじめに/practice/practice1.js
@@ -0,0 +1,13 @@
+var message1 = new Vue({
+    el: '#message1',
+    data: {
+        message: "ビュ〜.js"
+    }
+})
+
+var message2 = new Vue({
+    el: '#message2',
+    data: {
+        message: "ここ"
+    }
+})
